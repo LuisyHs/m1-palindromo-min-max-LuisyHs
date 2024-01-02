9b74b151c1b5c68b05a47d46beac0277c5ab1751
Luis Soares Brasil <llhenriquesoares@gmail.com>
2024-01-02 16:06:19 -0300

    Entrega Total do projeto

--- script.js
+++ script.js
@@ -24,8 +24,34 @@ function isPalindrome(str){
     }
     return verify;
 }
-let teste = [1,2];
-console.log(teste.length)
+
+
 function arrayMaxMin(arr){
+    let maiorValor = arr[0];
+    let menorValor;
+    let contador = 1;
+    let resultado = [];
+
+    if(arr[contador] > maiorValor){
+        menorValor = maiorValor;
+        maiorValor = arr[contador];
+        contador++;
+    }else{
+        menorValor = arr[contador];
+        contador++;
+    }
+
+    while(contador < arr.length){
+        if (arr[contador] > maiorValor){
+            maiorValor = arr[contador];
+        }else if (arr[contador] < menorValor ){
+            menorValor = arr[contador];
+        }
+        contador++;
+    }
+
+    resultado.unshift(maiorValor);
+    resultado.unshift(menorValor);
     
+    return resultado;
 }
