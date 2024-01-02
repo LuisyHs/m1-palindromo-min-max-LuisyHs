6ee83a3bfb2f5d44f3c05dac717212a0ebae28e0
Luis Soares Brasil <llhenriquesoares@gmail.com>
2024-01-02 14:53:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 function isPalindrome(str){
-    /* Seu código aqui */
-}
+    str = str.toUpperCase();
+    let variavelControle = parseInt(str.length/2);
+    let contador = 0;
+    let contraPeso = 1;
+    let verify = true;
 
+
+    while(contador < (variavelControle)){
+        if(str[contador]==" "){
+            contador++;
+            contraPeso--;
+        }
+        if(str[str.length-(contador+contraPeso)]==" "){
+            contraPeso++;
+        }
+
+        if(str[contador]==str[str.length-(contador+contraPeso)]){
+            verify=true;
+        }else{
+            verify=false;
+        }
+        contador++;
+    }
+    return verify;
+}
+let teste = [1,2];
+console.log(teste.length)
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    
 }
